123544433d1f5deb6b8ef4283cf5b1adab21b4b2
otherPOM <otherPOM@gmail.com>
2021-01-14 20:04:17 +0300

    Stage 2 done

--- Main.java
+++ Main.java
@@ -1,7 +1,29 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scan = new Scanner(System.in);
+    private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var message = scan.nextLine();
+        var key = scan.nextInt();
+        System.out.println(encrypt(message, key));
+    }
+
+    private static String encrypt(String message, int key) {
+        var sb = new StringBuilder();
+        for (char c : message.toCharArray()) {
+            if (c <= 'z' && c >= 'a') {
+                char newChar = (char) (c + key);
+                newChar = newChar < 'a' ? (char) (newChar + 26) :
+                        newChar > 'z' ? (char) (newChar - 26) : newChar;
+                sb.append(newChar);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 }
