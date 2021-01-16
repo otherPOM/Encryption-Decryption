3731612e6e7ec6c8595bd0aacfde07e37b94f422
otherPOM <otherPOM@gmail.com>
2021-01-16 12:55:42 +0300

    Stage 3 done

--- Main.java
+++ Main.java
@@ -4,26 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     private static final Scanner scan = new Scanner(System.in);
-    private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     public static void main(String[] args) {
+        var command = scan.nextLine();
         var message = scan.nextLine();
         var key = scan.nextInt();
-        System.out.println(encrypt(message, key));
+        switch (command) {
+            case "enc":
+                System.out.println(encrypt(message, key));
+                break;
+            case "dec":
+                System.out.println(decrypt(message, key));
+                break;
+        }
     }
 
     private static String encrypt(String message, int key) {
-        var sb = new StringBuilder();
-        for (char c : message.toCharArray()) {
-            if (c <= 'z' && c >= 'a') {
-                char newChar = (char) (c + key);
-                newChar = newChar < 'a' ? (char) (newChar + 26) :
-                        newChar > 'z' ? (char) (newChar - 26) : newChar;
-                sb.append(newChar);
-            } else {
-                sb.append(c);
-            }
-        }
-        return sb.toString();
+        return message.codePoints().map(c -> c + key).collect(StringBuilder::new,
+                StringBuilder::appendCodePoint, StringBuilder::append).toString();
+    }
+
+    private static String decrypt(String message, int key) {
+        return message.codePoints().map(c -> c - key).collect(StringBuilder::new,
+                StringBuilder::appendCodePoint, StringBuilder::append).toString();
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Encryption-Decryption https://hyperskill.org/projects/46
 ## Stage 1
 For the first stage, you need to manually encrypt the message "**we found a treasure!**" and print only the ciphertext (in lower case)
+
+## Stage 2
+Write a program that reads an English message and an integer number (key) from the standard input and shifts each letter by the specified number according to its order in the alphabet. If you reach the end of the alphabet, start back at the beginning (a follows z).
+
+## Stage 3
+In this stage, you need to support decryption in your program. The decryption is simply the inverse of encryption, following the same steps but reversing the order in which the keys are applied.
+
